97a23e9c6a25a6fc3fae19517e52d1c164485166
dev <dev@develop.com>
2019-09-11 11:06:49 +0530

    cahnges

--- application/views/Pages/contactus.php
+++ application/views/Pages/contactus.php
@@ -3,51 +3,52 @@ $this->load->view('layout/header');
 ?>
 <!--contannt section-->
 <app-contact-us _nghost-c15="" class="ng-star-inserted">
-   <section _ngcontent-c15="" class="sbPageTopSec servicesPgSec">
-      <div _ngcontent-c15="" class="container">
-         <h2 _ngcontent-c15="">Contact Us</h2>
-      </div>
-   </section>
+    <section _ngcontent-c15="" class="sbPageTopSec servicesPgSec">
+        <div _ngcontent-c15="" class="container">
+            <h2 _ngcontent-c15="">Contact Us</h2>
+        </div>
+    </section>
 
-   <section _ngcontent-c15="" class="cntctFrmCntSec">
-      <div _ngcontent-c15="" class="container">
-         <div _ngcontent-c15="" class="cntctFrmCnt">
-            <div _ngcontent-c15="" class="cntctFrmsCnt">
-               <div _ngcontent-c15="" class="cntctFrmsSbCnt">
-                  <h3 _ngcontent-c15="">Send Us a Message</h3>
-                  <form _ngcontent-c15="" novalidate="" class="ng-untouched ng-pristine ng-invalid">
-                     <input _ngcontent-c15="" formcontrolname="name" name="name" placeholder="Enter your full name" type="text" class="ng-untouched ng-pristine ng-invalid"><!----><input _ngcontent-c15="" formcontrolname="email" name="email" placeholder="Enter your email" type="email" value="" class="ng-untouched ng-pristine ng-invalid"><!----><!----><input _ngcontent-c15="" formcontrolname="phone" name="phone" placeholder="Enter your phone" type="text" value="" class="ng-untouched ng-pristine ng-invalid"><!----><!---->
-                     <textarea _ngcontent-c15="" formcontrolname="query" name="query" placeholder="Enter your query" class="ng-untouched ng-pristine ng-invalid"></textarea>
-                     <!----><!----><button _ngcontent-c15="" class="sendBtn" type="submit">Send</button>
-                  </form>
-               </div>
+    <section _ngcontent-c15="" class="cntctFrmCntSec">
+        <div _ngcontent-c15="" class="container">
+            <div _ngcontent-c15="" class="cntctFrmCnt">
+                <div _ngcontent-c15="" class="cntctFrmsCnt">
+                    <div _ngcontent-c15="" class="cntctFrmsSbCnt">
+                        <h3 _ngcontent-c15="">Send Us a Message</h3>
+                        <form _ngcontent-c15="" novalidate="" class="ng-untouched ng-pristine ng-invalid">
+                            <input _ngcontent-c15="" formcontrolname="name" name="name" placeholder="Enter your full name" type="text" class="ng-untouched ng-pristine ng-invalid"><!----><input _ngcontent-c15="" formcontrolname="email" name="email" placeholder="Enter your email" type="email" value="" class="ng-untouched ng-pristine ng-invalid"><!----><!----><input _ngcontent-c15="" formcontrolname="phone" name="phone" placeholder="Enter your phone" type="text" value="" class="ng-untouched ng-pristine ng-invalid"><!----><!---->
+                            <textarea _ngcontent-c15="" formcontrolname="query" name="query" placeholder="Enter your query" class="ng-untouched ng-pristine ng-invalid"></textarea>
+                            <!----><!----><button _ngcontent-c15="" class="sendBtn" type="submit">Send</button>
+                        </form>
+                    </div>
+                </div>
+                <div _ngcontent-c15="" class="cntctAddress">
+                    <h3 _ngcontent-c15="">Drop in our Office</h3>
+                    <p _ngcontent-c15=""> We would love to hear from you. Drop us a Line or Call with your queries, feedback or concerns. </p>
+                    <ul _ngcontent-c15="" class="adrsCnt">
+                        <li _ngcontent-c15="">
+                            <i _ngcontent-c15="" class="fas fa-map-marker-alt"></i>
+                            <p _ngcontent-c15="">C-44,Shop No.2,<br _ngcontent-c15="">Street no. 15, Mabhu Vihar <br/>New Delhi,  110092 India</p>
+                        </li>
+                        <li _ngcontent-c15=""><i _ngcontent-c15="" class="fas fa-envelope"></i><a _ngcontent-c15="" href="mailto:support@styletreatstudio.com">support@styletreatstudio.com</a></li>
+                        <li _ngcontent-c15="">
+                            <i _ngcontent-c15="" class="fas fa-mobile-alt"></i>
+                            <p _ngcontent-c15="">098736 77954</p>
+                        </li>
+                    </ul>
+                    <ul _ngcontent-c15="" class="scialAccnts">
+                        <p _ngcontent-c15="">Follow Us</p>
+                        <li _ngcontent-c15="" class="fb"><a _ngcontent-c15="" href="#"><i _ngcontent-c15="" class="fab fa-facebook-f"></i></a></li>
+                        <li _ngcontent-c15="" class="tw"><a _ngcontent-c15="" href="#"><i _ngcontent-c15="" class="fab fa-twitter"></i></a></li>
+                        <li _ngcontent-c15="" class="inst"><a _ngcontent-c15="" href="#"><i _ngcontent-c15="" class="fab fa-instagram"></i></a></li>
+                        <li _ngcontent-c15="" class="pint"><a _ngcontent-c15="" href="#"><i _ngcontent-c15="" class="fab fa-pinterest-p"></i></a></li>
+                    </ul>
+                </div>
             </div>
-            <div _ngcontent-c15="" class="cntctAddress">
-               <h3 _ngcontent-c15="">Drop in our Office</h3>
-               <p _ngcontent-c15=""> We would love to hear from you. Drop us a Line or Call with your queries, feedback or concerns. </p>
-               <ul _ngcontent-c15="" class="adrsCnt">
-                  <li _ngcontent-c15="">
-                     <i _ngcontent-c15="" class="fas fa-map-marker-alt"></i>
-                     <p _ngcontent-c15="">C-44,Shop No.2,<br _ngcontent-c15="">Street no. 15, Mabhu Vihar <br/>New Delhi,  110092 India</p>
-                  </li>
-                  <li _ngcontent-c15=""><i _ngcontent-c15="" class="fas fa-envelope"></i><a _ngcontent-c15="" href="mailto:support@styletreatstudio.com">support@styletreatstudio.com</a></li>
-                  <li _ngcontent-c15="">
-                     <i _ngcontent-c15="" class="fas fa-mobile-alt"></i>
-                     <p _ngcontent-c15="">098736 77954</p>
-                  </li>
-               </ul>
-               <ul _ngcontent-c15="" class="scialAccnts">
-                  <p _ngcontent-c15="">Follow Us</p>
-                  <li _ngcontent-c15="" class="fb"><a _ngcontent-c15="" href="#"><i _ngcontent-c15="" class="fab fa-facebook-f"></i></a></li>
-                  <li _ngcontent-c15="" class="tw"><a _ngcontent-c15="" href="#"><i _ngcontent-c15="" class="fab fa-twitter"></i></a></li>
-                  <li _ngcontent-c15="" class="inst"><a _ngcontent-c15="" href="#"><i _ngcontent-c15="" class="fab fa-instagram"></i></a></li>
-                  <li _ngcontent-c15="" class="pint"><a _ngcontent-c15="" href="#"><i _ngcontent-c15="" class="fab fa-pinterest-p"></i></a></li>
-               </ul>
-            </div>
-         </div>
-      </div>
-   </section>
- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        </div>
+    </section>
+    <section _ngcontent-c15="" class="cntctFrmCntSec">
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <!-- STS Display ad 2 -->
         <ins class="adsbygoogle"
              style="display:block"
@@ -58,6 +59,7 @@ $this->load->view('layout/header');
         <script>
             (adsbygoogle = window.adsbygoogle || []).push({});
         </script>
+    </section>
 </app-contact-us>
 <!--end of contant section-->
 
