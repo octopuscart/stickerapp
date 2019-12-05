d7d2f8341fd179566572bc4bfdb5c5bb5e6f17aa
dev <dev@develop.com>
2019-12-06 01:16:00 +0530

    changes

--- application/views/Pages/aboutus.php
+++ application/views/Pages/aboutus.php
@@ -15,15 +15,10 @@ $this->load->view('layout/header');
             <div _ngcontent-c14="" class="container">
                 <div _ngcontent-c14="" class="abtTxt">
                     <p _ngcontent-c14=""> 
-                        Style Treat Studio- Tripti Garg initiated the Style Treat as a passion to follow her long lost dream to beauty, which began on 1st May’2018. She established the first branch in Patparganj and began on a small scale which has only grown to new heights ever since. She went on to curate her dream into reality by building two more branches of the same brand and strengthening her roots even deeper in the beauty world. She is a certified professional make-up artist from ‘STAR ACADEMY’ by Ashmeen Munjaal. She has a team of experts lined up to serve to the best of their ability and to accommodate all your demands. They continue to explore the world of the beauty with hands on experience in hair, make-up and beauty. They create customized packages for their clients to meet all their needs. 
-                    </p>
+Style Treat Studio- Tripti Garg initiated the Style Treat as a passion to follow her long lost dream to beauty, which began on 1st May’2018. She established the first branch in Patparganj and began on a small scale which has only grown to new heights ever since. She went on to curate her dream into reality by building two more branches of the same brand and strengthening her roots even deeper in the beauty world. She is a certified professional make-up artist from ‘STAR ACADEMY’ by Ashmeen Munjaal. She has a team of experts lined up to serve to the best of their ability and to accommodate all your demands. They continue to explore the world of the beauty with hands on experience in hair, make-up and beauty. They create customized packages for their clients to meet all their needs.                     </p>
                     <p _ngcontent-c14=""> 
-                        She set-up the first branch at Patparganj which did not serve her ambitions to the core of it so as a young entrepreneur she decided to enhance her services by providing the pick and drop facilities to her clients. She later realised that this was not the zenith of her ambition so she decided to get ahead of it and established a new branch in Madhu Vihar but this still did not satiate her she also wanted her clients to be pampered properly when they get ready for their wedding she would want them to lavishly sit in the car parked right at the door of salon which wasn’t possible at Madhu Vihar so she further spread her wings to engender a branch at Mayur Vihar which could serve the purpose.
-                    </p>
-                    <p>
-                        Do check out our latest packages on our STS League page or become part of our STS League whatsapp group.
-
-                    </p>
+She set-up the first branch at Patparganj which did not serve her ambitions to the core of it so as a young entrepreneur she decided to enhance her services by providing the pick and drop facilities to her clients. She later realised that this was not the zenith of her ambition so she decided to get ahead of it and established a new branch in Madhu Vihar but this still did not satiate her. She also wanted her clients to be pampered properly when they get ready for their wedding. She would want them to lavishly sit in the car parked right at the door of salon which wasn’t possible at Madhu Vihar so she further spread her wings to engender a branch at Mayur Vihar which could serve the purpose.                    </p>
+                    
                 </div>
             </div>
         </section>
